838e004d0c4046a500c397a6507aa6fd93fb89f0
yibrino <yibrahmehari128@gmail.com>
2026-01-04 00:36:56 +0100

    Car Steering System Animation added

--- Assets/Scripts/PlayerCarController.cs
+++ Assets/Scripts/PlayerCarController.cs
@@ -21,10 +21,17 @@ public class PlayerCarController : MonoBehaviour
     public float breakingForce = 3000f;
     private float presentBreakForce = 0f;
     private float presentAcceleration = 0f;
+    [Header("Car Steering")]
+    public float wheelsTorque=35f;
+    public float presentTurnAngle=0f;
+
+
+
 
     private void Update()
     {
         MoveCar();
+        CarSteering();
     }
 
     private void MoveCar()
@@ -36,5 +43,11 @@ public class PlayerCarController : MonoBehaviour
         backRightWheelCollider.motorTorque = presentAcceleration;
 
         presentAcceleration = accelerationForce * Input.GetAxis("Vertical");
+    }
+    private void CarSteering()
+    {
+        presentTurnAngle=wheelsTorque*Input.GetAxis("Horizontal");
+        frontLeftWheelCollider.steerAngle= presentTurnAngle;
+        frontRightWheelCollider.steerAngle=presentTurnAngle;
     }
 }
