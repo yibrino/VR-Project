cf087f7b11b3d7257025611b13a0956cb86b3ee5
yibrino <yibrahmehari128@gmail.com>
2026-01-04 00:00:30 +0100

    waypoint system added

--- Assets/Editor/WaypointManagerWindow.cs
+++ Assets/Editor/WaypointManagerWindow.cs
@@ -10,4 +10,57 @@ public class WaypointManagerWindow : EditorWindow
     {
         GetWindow<WaypointManagerWindow>("Waypoints Editor Tools");
     }
+
+    public Transform waypointOrigin;
+
+    private void OnGUI()
+    {
+        SerializedObject obj = new SerializedObject(this);
+
+        EditorGUILayout.PropertyField(obj.FindProperty("waypointOrigin"));
+
+        if (waypointOrigin == null)
+        {
+            EditorGUILayout.HelpBox("Please assign a Waypoint origin tranform. ", MessageType.Warning);
+        }
+        else
+        {
+            EditorGUILayout.BeginVertical("box");
+            CreateButtons();
+            EditorGUILayout.EndVertical();
+        }
+
+        obj.ApplyModifiedProperties();
+    }
+
+    void CreateButtons()
+    {
+        if (GUILayout.Button("Create Waypoint"))
+        {
+            CreateWaypoint();
+        }
+    }
+
+    void CreateWaypoint()
+    {
+        GameObject waypointObject =
+            new GameObject("Waypoint " + waypointOrigin.childCount, typeof(Waypoint));
+        waypointObject.transform.SetParent(waypointOrigin, false);
+
+        Waypoint waypoint = waypointObject.GetComponent<Waypoint>();
+
+        if (waypointOrigin.childCount > 1)
+        {
+            waypoint.previousWaypoint = waypointOrigin
+                .GetChild(waypointOrigin.childCount - 2)
+                .GetComponent<Waypoint>();
+
+            waypoint.previousWaypoint.nextWaypoint = waypoint;
+
+            waypoint.transform.position = waypoint.previousWaypoint.transform.position;
+            waypoint.transform.forward = waypoint.previousWaypoint.transform.forward;
+        }
+
+        Selection.activeGameObject = waypoint.gameObject;
+    }
 }
