336777446be19b8037dfaf02cffb6798eff04352
yibrino <yibrahmehari128@gmail.com>
2026-02-04 17:07:55 +0100

    Waypoint DrawSphere

--- Assets/Editor/WaypointEditor.cs
+++ Assets/Editor/WaypointEditor.cs
@@ -9,18 +9,31 @@ public class WaypointEditor
     [DrawGizmo(GizmoType.NonSelected | GizmoType.Selected | GizmoType.Pickable)]
     public static void OnDrawSceneGizmos(Waypoint waypoint, GizmoType gizmoType)
     {
+        // Color based on selection
         if ((gizmoType & GizmoType.Selected) != 0)
-        {
             Gizmos.color = Color.blue;
-        }
         else
-        {
             Gizmos.color = Color.blue * 0.5f;
+
+        // Draw waypoint sphere
+        Gizmos.DrawSphere(waypoint.transform.position, 0.1f);
+
+        // Draw waypoint width line
+        Gizmos.color = Color.white;
+        Gizmos.DrawLine(
+            waypoint.transform.position + (waypoint.transform.right * waypoint.waypointWidth / 2f),
+            waypoint.transform.position - (waypoint.transform.right * waypoint.waypointWidth / 2f)
+        );
+
+        // Now draw a line from previous to next waypoint (if you have references set up)
+        if (waypoint.previousWaypoint != null)
+        {
+            Gizmos.DrawLine(waypoint.previousWaypoint.transform.position, waypoint.transform.position);
         }
 
-        // NOTE: radius should be positive
-        Gizmos.DrawSphere(waypoint.transform.position, 1f);
-
-        Gizmos.color = Color.white;
+        if (waypoint.nextWaypoint != null)
+        {
+            Gizmos.DrawLine(waypoint.transform.position, waypoint.nextWaypoint.transform.position);
+        }
     }
 }
