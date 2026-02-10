06b914bff44049733c1867885a46827323371e40
yibrino <yibrahmehari128@gmail.com>
2026-02-10 11:31:11 +0100

    Camera animation added

--- Assets/Scripts/CarSelection.cs
+++ Assets/Scripts/CarSelection.cs
@@ -11,19 +11,51 @@ public class CarSelection : MonoBehaviour
     public Button previousButton;
 
     private int currentCar;
+    private GameObject[] carList;
 
     private void Awake()
     {
         chooseCar(0);
     }
 
+    private void Start()
+    {
+        // Load previously selected car (default to 0 if not found)
+        currentCar = PlayerPrefs.GetInt("CarSelected", 0);
+
+        // Feeding car models to carList array
+        carList = new GameObject[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            carList[i] = transform.GetChild(i).gameObject;
+        }
+
+        // Deactivate all cars first
+        foreach (GameObject go in carList)
+        {
+            go.SetActive(false);
+        }
+
+        // Activate the selected car
+        if (currentCar >= 0 && currentCar < carList.Length && carList[currentCar] != null)
+        {
+            carList[currentCar].SetActive(true);
+        }
+
+        // Make sure buttons reflect current state
+        chooseCar(currentCar);
+    }
+
     private void chooseCar(int index)
     {
         currentCar = index;
 
+        // Update button interactability
         previousButton.interactable = (currentCar != 0);
         nextButton.interactable = (currentCar != transform.childCount - 1);
 
+        // Show only the selected car
         for (int i = 0; i < transform.childCount; i++)
         {
             transform.GetChild(i).gameObject.SetActive(i == index);
@@ -33,6 +65,16 @@ public class CarSelection : MonoBehaviour
     public void switchCar(int switchCars)
     {
         currentCar += switchCars;
+
+        // Optional: clamp to valid range (prevents out-of-bounds)
+        currentCar = Mathf.Clamp(currentCar, 0, transform.childCount - 1);
+
         chooseCar(currentCar);
+    }
+
+    public void playGame()
+    {
+        PlayerPrefs.SetInt("CarSelected", currentCar);
+        SceneManager.LoadScene("scene_day");
     }
 }
