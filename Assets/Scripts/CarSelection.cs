628d350622bfb0afe852914ad60f3168ad0359c7
yibrino <yibrahmehari128@gmail.com>
2026-02-10 01:30:17 +0100

    Previous and next interactable added

--- Assets/Scripts/CarSelection.cs
+++ Assets/Scripts/CarSelection.cs
@@ -10,10 +10,20 @@ public class CarSelection : MonoBehaviour
     public Button nextButton;
     public Button previousButton;
 
-    private int currentCar = 0;
+    private int currentCar;
+
+    private void Awake()
+    {
+        chooseCar(0);
+    }
 
     private void chooseCar(int index)
     {
+        currentCar = index;
+
+        previousButton.interactable = (currentCar != 0);
+        nextButton.interactable = (currentCar != transform.childCount - 1);
+
         for (int i = 0; i < transform.childCount; i++)
         {
             transform.GetChild(i).gameObject.SetActive(i == index);
